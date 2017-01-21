434e00ee85591b912b37f25f6738c1f34d3d1094
coreyarch1234 <harrilal@mit.edu>
2017-01-21 12:14:37 -0800

    completed divmod problem

--- hacker_rank_problems/Mod-Divmod.py
+++ hacker_rank_problems/Mod-Divmod.py
@@ -1,2 +1,12 @@
 # Mod-Divmod
 # https://www.hackerrank.com/challenges/python-mod-divmod
+
+from __future__ import division
+a = int(raw_input())
+b = int(raw_input())
+
+first = a // b
+second = a % b
+print(first)
+print(second)
+print((first, second))
