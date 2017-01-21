3441ac33c88cac4a1ad9622b03b5fb4db403ad5f
coreyarch1234 <harrilal@mit.edu>
2017-01-21 12:23:50 -0800

    completed power mod problem

--- hacker_rank_problems/Power-Mod-Power.py
+++ hacker_rank_problems/Power-Mod-Power.py
@@ -1,1 +1,10 @@
 # https://www.hackerrank.com/challenges/python-power-mod-power
+
+a = int(raw_input())
+b = int(raw_input())
+m = int(raw_input())
+
+first = pow(a,b)
+second = pow(a,b,m)
+print(first)
+print(second)
