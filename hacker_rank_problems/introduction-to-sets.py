d86a6ae9babc02081d52b99798353329d6e422ba
coreyarch1234 <harrilal@mit.edu>
2017-01-22 16:39:58 -0800

    finished introduction to set challenge

---
+++ hacker_rank_problems/introduction-to-sets.py
@@ -0,0 +1,16 @@
+# https://www.hackerrank.com/challenges/py-introduction-to-sets
+from __future__ import division
+def average(array):
+    return((sum(set(array)))/len(set(array)))
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    n = int(raw_input())
+    arr = map(int, raw_input().split())
+    result = average(arr)
+    print result
