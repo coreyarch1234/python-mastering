34b21eb6c73d4c8ab37095c210a8d5b59325afad
coreyarch1234 <harrilal@mit.edu>
2017-01-24 06:16:48 -0800

    added the rest of the animal classes

--- ms-oop/zoo_problems.py
+++ ms-oop/zoo_problems.py
@@ -1,7 +1,65 @@
-import sys
 
-# Implement the sleep function here
-def sleep(name):
-    print(name + " sleeps for 8 hours")
+class Animal(object):
+    def __init__(self, name, favoriteFood):
+        self.name = name
+        self.favoriteFood = favoriteFood
 
-sleep("Tigger")
+    def sleep(self):
+        print(self.name + " sleeps for 8 hours")
+
+    def eat(self, food):
+        print(self.name + " eats " + food)
+        if food == self.favoriteFood:
+            print("YUM! " + self.name + " wants more " + food)
+
+class Tiger(Animal):
+    # Implement the initializer method here
+    def __init__(self, name):
+        Animal.__init__(self, name, "meat")
+
+class Bear(Animal):
+    def __init__(self, name):
+        Animal.__init__(self, name, "fish")
+
+    def sleep(self):
+        print(self.name + " hibernates for 4 months")
+
+# Implement the Unicorn class here as a subclass of Animal
+# Hint: Implement the initializer method and override the sleep method
+class Unicorn(Animal):
+    def __init__(self, name):
+        Animal.__init__(self, name, "marshmallows")
+
+    def sleep(self):
+        print(self.name + " sleeps in a cloud")
+
+
+# Implement the Giraffe class here as a subclass of Animal
+# Hint: Implement the initializer method and override the eat method
+class Giraffe(Animal):
+    def __init__(self, name):
+        Animal.__init__(self, name, "leaves")
+
+    def eat(self, food):
+        print(self.name + " eats " + food)
+        if food == self.favoriteFood:
+            print("YUM! " + self.name + " wants more " + food)
+        else:
+            print("YUCK! " + self.name + " spits out " + food)
+
+
+# Implement the Bee class here as a subclass of Animal
+# Hint: Implement the initializer method and override the sleep and eat methods
+class Bee(Animal):
+    def __init__(self, name):
+        Animal.__init__(self, name, "pollen")
+
+    def sleep(self):
+        print(self.name + " never sleeps")
+
+    def eat(self, food):
+        print(self.name + " eats " + food)
+        if food == self.favoriteFood:
+            print("YUM! " + self.name + " wants more " + food)
+        else:
+            print("YUCK! " + self.name + " spits out " + food)
